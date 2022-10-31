df5391e2f7a0b0fc0a0772fde4c85875a60cab62
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-11-01 01:36:25 +0700

    Create AttendanceDBContext.java
    create AttendanceDBContext

---
+++ MyProject/src/java/dal/AttendanceDBContext.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Attendance;
+import model.Session;
+import model.Student;
+
+/**
+ *
+ * @author admin
+ */
+public class AttendanceDBContext extends DBContext<Attendance> {
+
+    public ArrayList<Attendance> getAttsBySessionID(int sesid) {
+        ArrayList<Attendance> atts = new ArrayList<>();
+        try {
+            String sql = "SELECT std.stdid, std.stdname,std.imageURL , ses.sesid, ISNULL(a.present,0) present , ISNULL(a.[description],'') [description]\n"
+                    + "FROM [Session] ses \n"
+                    + "INNER JOIN [Group] g ON ses.gid = g.gid\n"
+                    + "INNER JOIN Student_Group sg ON g.gid = sg.gid\n"
+                    + "INNER JOIN Student std ON sg.stdid = std.stdid\n"
+                    + "LEFT JOIN Attandance a ON std.stdid = a.stdid AND ses.sesid = a.sesid\n"
+                    + "WHERE ses.sesid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, sesid);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Attendance att = new Attendance();
+                Student s = new Student();
+                att.setStudent(s);
+                Session ses = new Session();
+                att.setSession(ses);
+                att.setPresent(rs.getBoolean("present"));
+                att.setDescription(rs.getString("description"));
+                s.setId(rs.getInt("stdid"));
+                s.setName(rs.getString("stdname"));
+                s.setImageUrl(rs.getString("imageURL"));
+                ses.setId(sesid);
+                atts.add(att);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AttendanceDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return atts;
+    }
+
+    @Override
+    public void insert(Attendance model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Attendance model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Attendance model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Attendance get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Attendance> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
