8fbb227045513bb67a1de80be92a4b00b3cdbc55
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-11-02 10:26:35 +0700

    create takeatt.jsp, TakeAttController, SessionDBContext
    create takeatt.jsp, TakeAttController, SessionDBContext . Write method get Session by sesID in SessionDBContext.java

--- MyProject/src/java/dal/SessionDBContext.java
+++ MyProject/src/java/dal/SessionDBContext.java
@@ -4,10 +4,103 @@
  */
 package dal;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Group;
+import model.Lecturer;
+import model.Room;
+import model.Session;
+import model.Subject;
+import model.TimeSlot;
+
 /**
  *
  * @author admin
  */
-public class SessionDBContext {
-    
+public class SessionDBContext extends DBContext<Session> {
+
+    @Override
+    public void insert(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Session get(int id) {
+        try {
+            String sql = "Select ses.sesid ,ses.[date], ses.[index], ses.attanded, \n"
+                    + "g.gid,g.gname,\n"
+                    + "sub.subid,sub.subname,\n"
+                    + "l.lid,l.lname,\n"
+                    + "r.rid,r.rname,\n"
+                    + "t.tid,t.description\n"
+                    + "from [Session] ses\n"
+                    + "INNER JOIN [Group] g ON ses.gid = g.gid\n"
+                    + "INNER JOIN Lecturer l ON l.lid = ses.lid\n"
+                    + "INNER JOIN TimeSlot t ON t.tid = ses.tid\n"
+                    + "INNER JOIN Room r ON r.rid = ses.rid\n"
+                    + "INNER JOIN [Subject] sub ON sub.subid = g.subid\n"
+                    + "WHERE ses.sesid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Session ses = new Session();
+                ses.setId(rs.getInt("sesid"));
+                ses.setDate(rs.getDate("date"));
+                ses.setIndex(rs.getInt("index"));
+                ses.setAttanded(rs.getBoolean("attended"));
+
+                Group g = new Group();
+                ses.setGroup(g);
+                g.setId(rs.getInt("gid"));
+                g.setName(rs.getString("gname"));
+
+                Room r = new Room();
+                ses.setRoom(r);
+                r.setId(rs.getInt("rid"));
+                r.setName(rs.getString("rname"));
+
+                TimeSlot t = new TimeSlot();
+                ses.setSlot(t);
+                t.setId(rs.getInt("tid"));
+                t.setDescription(rs.getString("description"));
+
+                Lecturer l = new Lecturer();
+                ses.setLecturer(l);
+                l.setId(rs.getInt("lid"));
+                l.setName(rs.getString("lname"));
+
+                Subject sub = new Subject();
+                g.setSubject(sub);
+                sub.setId(rs.getInt("subid"));
+                sub.setName(rs.getString("subname"));
+
+                return ses;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<Session> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
---
+++ MyProject/web/view/lecturer/takeatt.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : takeatt
+    Created on : Nov 2, 2022, 8:56:13 AM
+    Author     : admin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action="takeatt" method="POST">
+            <input type="hidden" name="sesid" value="${param.id}"/>    
+            <table border="1">
+                
+            </table>
+        </form>
+    </body>
+</html>
