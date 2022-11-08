341f595bf047a51e1d91a9af6ad2ebd6210c4fe9
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-11-08 10:14:47 +0700

    create TimeTableController, LecturerDBContext, TimeSlotDBContext
    create DateTimeHelper , create TimeTableController, LecturerDBContext, TimeSlotDBContext .
    Create method get LECTURER by lecturer id

--- MyProject/src/java/controller/lecturer/TakeAttController.java
+++ MyProject/src/java/controller/lecturer/TakeAttController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.lecturer;
 
 import dal.AttendanceDBContext;
@@ -17,18 +16,18 @@ import java.util.ArrayList;
 import model.Attendance;
 import model.Session;
 import model.Student;
+import util.DateTimeHelper;
 
 /**
  *
  * @author admin
  */
 public class TakeAttController extends HttpServlet {
-   
-    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -36,22 +35,28 @@ public class TakeAttController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         int sesid = Integer.parseInt(request.getParameter("id"));
         AttendanceDBContext attDB = new AttendanceDBContext();
         ArrayList<Attendance> atts = attDB.getAttsBySessionID(sesid);
         request.setAttribute("atts", atts);
-        
+
         SessionDBContext sesDB = new SessionDBContext();
         Session ses = sesDB.get(sesid);
         request.setAttribute("ses", ses);
-        request.getRequestDispatcher("../view/lecturer/takeatt.jsp").forward(request, response);
-        
-                
-    } 
+//        if (DateTimeHelper.getDaystoCurrent(ses.getDate()) >= 2) {
+//            response.getWriter().println("this session is out of date");
+//        } else if (DateTimeHelper.getDaystoCurrent(ses.getDate()) < 0) {
+//            response.getWriter().println("this session is not yet started");
+//        } else {
+//        }
+            request.getRequestDispatcher("../view/lecturer/takeatt.jsp").forward(request, response);
 
-    /** 
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -59,10 +64,10 @@ public class TakeAttController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         Session ses = new Session();
         ses.setId(Integer.parseInt(request.getParameter("sesid")));
-        
+
         String[] stdids = request.getParameterValues("stdid");
         for (String stdid : stdids) {
             Attendance a = new Attendance();
@@ -70,23 +75,24 @@ public class TakeAttController extends HttpServlet {
             a.setStudent(s);
             a.setSession(ses);
             s.setId(Integer.parseInt(stdid));
-            a.setPresent(request.getParameter("present"+stdid).equals("present"));
-            a.setDescription(request.getParameter("description"+stdid));
+            a.setPresent(request.getParameter("present" + stdid).equals("present"));
+            a.setDescription(request.getParameter("description" + stdid));
             ses.getAtts().add(a);
         }
-        
+
         SessionDBContext sesDB = new SessionDBContext();
         sesDB.updateAttendance(ses);
-        response.sendRedirect("takeatt?id="+ses.getId());
+        response.sendRedirect("takeatt?id=" + ses.getId());
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-    
+
 }
