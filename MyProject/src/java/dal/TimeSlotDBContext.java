81c5aaa81c5efb4fa3b3f85627d9322a738949b2
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-11-08 10:37:15 +0700

    Update TimeSlotDBContext.java
    create method list to get all information about TimeSlot

--- MyProject/src/java/dal/TimeSlotDBContext.java
+++ MyProject/src/java/dal/TimeSlotDBContext.java
@@ -4,10 +4,58 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.TimeSlot;
+
 /**
  *
  * @author admin
  */
-public class TimeSlotDBContext {
-    
+public class TimeSlotDBContext extends DBContext<TimeSlot> {
+
+    @Override
+    public void insert(TimeSlot model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(TimeSlot model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(TimeSlot model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TimeSlot get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<TimeSlot> list() {
+        ArrayList<TimeSlot> timeslots = new ArrayList<>();
+        try {
+            String sql = "SELECT tid,[description] \n"
+                    + "FROM TimeSlot";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                TimeSlot t = new TimeSlot();
+                t.setId(rs.getInt("tid"));
+                t.setDescription(rs.getString("description"));
+                timeslots.add(t);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TimeSlotDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return timeslots;
+    }
+
 }
